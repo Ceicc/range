02a22eee7ef4eac435677b6f2f69011513e197d3
husam <husam999h@gmail.com>
2022-04-16 23:03:58 +0300

    done converting to typescript

--- index.ts
+++ index.ts
@@ -1,71 +1,34 @@
-const
-fs = require("fs"),
-{ pipeline } = require("stream"),
-{ promisify } = require("util"),
-{ contentType } = require("mime-types"),
-optionsChecker = require("@ceicc/options-checker"),
-{ URL } = require("url"),
-{ IncomingMessage, ServerResponse } = require("http"),
+import { createReadStream, promises } from "fs"
+import { pipeline } from "stream"
+import { promisify } from "util"
+import { contentType } from "mime-types"
+import optionsChecker from "@ceicc/options-checker"
+import { URL } from "url"
+import type { IncomingMessage, ServerResponse } from "http"
+import type { NextFunction } from "express"
 
 // Im not going to use `stream/promises` liberary because it was added in
 // version 15.0, Not all hosting providers support that version (including mine)
-pipelinePromised = promisify(pipeline),
-
-nextTick = () => new Promise(r => process.nextTick(r))
+const pipelinePromised = promisify(pipeline)
 
 
-module.exports = range
+export default range
 
 
-/**
- * @typedef {object} options
- *
- * @property {number}   [maxAge] caching period in seconds - default `10800` (3 hours)
- *
- * @property {boolean}  [etag] add Etag header - default `true`
- *
- * @property {boolean}  [lastModified] add `last-modified` header - default `true`
- *
- * @property {boolean}  [conditional] whether to respect conditional requests or not - default `true`
- *
- * @property {boolean}  [range] accept range requests - default `true`
- *
- * @property {string|boolean} [notFound] a handler for non existing files
- *
- * `notFound: false` `next` will be called.
- *
- * `notFound: true` empty body with response code '404' will be sent (default).
- *
- * `notFound: <string>` send a file with response code '404', the given string is the path to file.
- *
- *    if the path doesn't led to a file, `next` will be called.
- *
- *    ***Note:*** The path is relative to the `baseDir` path.
- *
- * @property {boolean|Array<string>} [implicitIndex] Check for index files if the request path is a directory - default: `true`
- *
- *    Pass an array of extensions to check against. e.g. _`["html", "css"]`_
- *
- *    Or simply pass `true` to check for html extension only
- *
- * @property {string} [baseDir] the base dirctory - default: `'.'`
- *
- * @property {boolean} [hushErrors] Whether to ignore errors and reply with status code `500`, or pass the error to `next` function - default: `false`
- *
- * @property {boolean} [trailingSlash] Redirect directory requests to add trailing slash - default `true`
- *
- *    disabling this option will led to relative path issues. [see #9](https://github.com/Ceicc/range/issues/9)
- *
- *    `implicitIndex` must be `true`
- */
+type options = {
+  baseDir?: string,
+  hushErrors?: boolean,
+  conditional?: boolean,
+  range?: boolean,
+  maxAge?: number,
+  etag?: boolean,
+  lastModified?: boolean,
+  notFound?: boolean|string,
+  implicitIndex?: boolean|Array<string>,
+  trailingSlash?: boolean,
+}
 
-
-/**
- * @param {options} [options] configuration object
- * @returns {Function} Middleware function
- */
-
-function range(options = {}) {
+function range(options: options = {}) {
 
   optionsChecker(options, {
     baseDir:        { default: '.',   type: "string"  },
@@ -81,34 +44,19 @@ function range(options = {}) {
   })
 
 
-  /**
-   * @param {IncomingMessage} req request object
-   * @param {ServerResponse} res response object
-   * @param {Function} next errors handler function
-   */
-  return async function rangeMiddleware(req, res, next = console.error) {
-
-    if (!(req instanceof IncomingMessage)) {
-      await nextTick()  // To make it truly asynchronous
-      next(TypeError("Request object is not an instance of IncomingMessage"))
-    }
-
-    if (!(res instanceof ServerResponse)) {
-      await nextTick()  // To make it truly asynchronous
-      next(TypeError("Response object is not an instance of ServerResponse"))
-    }
+  return async function rangeMiddleware(req: IncomingMessage, res: ServerResponse, next: NextFunction): Promise<void> {
 
 
-    req.pathname = new URL(`https://example.com${req.url}`).pathname
+    const { pathname } = new URL(`https://example.com${req.url}`)
 
     try {
 
       // Using `var` to get function scope
-      var stat = await fs.promises.stat(options.baseDir + req.pathname)
+      var stat = await promises.stat(options.baseDir + pathname)
 
-    } catch (error) {
+    } catch (error: any) {
 
-      if (error.code === "ENOENT") {
+      if (error?.code === "ENOENT") {
 
         if (options.notFound === true)
           return forgetAboutIt(res, 404)
@@ -134,9 +82,9 @@ function range(options = {}) {
       if (!options.implicitIndex)
         return forgetAboutIt(res, 404)
 
-      if (options.trailingSlash && !hasTrailingSlash(req.pathname)) {
+      if (options.trailingSlash && !hasTrailingSlash(pathname)) {
         res.statusCode = 301
-        res.setHeader("Location", req.pathname + "/")
+        res.setHeader("Location", pathname + "/")
         res.end()
         return
       }
@@ -149,13 +97,13 @@ function range(options = {}) {
       else if (options.implicitIndex === true)
         extensions.add("html")
 
-      const directory = await fs.promises.readdir(options.baseDir + req.pathname)
+      const directory = await promises.readdir(options.baseDir + pathname)
 
       for (const extension of extensions) {
         if (!directory.includes(`index.${extension}`))
           continue
 
-        req.url = `${req.pathname}/index.${extension}`
+        req.url = `${pathname}/index.${extension}`
 
         return rangeMiddleware(req, res, next)
       }
@@ -171,19 +119,30 @@ function range(options = {}) {
     options.maxAge        && res.setHeader("cache-control", `max-age=${options.maxAge}`)
     options.range         && res.setHeader("accept-ranges", "bytes") // Hint to the browser range is supported
 
-    res.setHeader("content-type", contentType(req.pathname.split(".").pop()))
+    const extension = pathname.split(".").pop()
+
+    if (extension) {
+      const contentTypeHeader = contentType(extension)
+      typeof contentTypeHeader === "string" && res.setHeader("content-type", contentTypeHeader)
+    }
+
+
     res.setHeader("content-length", stat.size)
 
-    // check conditional request, calclute a diff up to 2 sec because browsers sends seconds and javascript uses milliseconds
-    if ( options.conditional && (
-      req.headers["if-none-match"] === etag ||
+    // check conditional requests
+    if ( options.conditional) {
 
-      // No need to check if the header exist because `Date.parse` will return `NaN` to falsy inputs,
-      // any arithmetic to `NaN` will result in `NaN`,
-      // and any compartion to `NaN` will result in `false`
-      ( Date.parse(req.headers["if-modified-since"]) - stat.mtime.getTime() ) >= -2000 )
-    )
-      return forgetAboutIt(res, 304)
+      const ifNoneMatch = req.headers["if-none-match"]
+
+      const ifModifiedSince = req.headers["if-modified-since"]
+
+      if (
+        ifNoneMatch === etag ||
+        ifModifiedSince && ( Date.parse(ifModifiedSince) - stat.mtime.getTime() ) >= -2000
+      )
+        return forgetAboutIt(res, 304)
+
+    }
 
     if (options.range && req.headers["range"]) {
 
@@ -193,7 +152,7 @@ function range(options = {}) {
 
         try {
 
-          await streamIt(options.baseDir + req.pathname, res)
+          await streamIt(options.baseDir + pathname, res)
 
         } catch (error) {
 
@@ -204,16 +163,21 @@ function range(options = {}) {
         return
       }
 
-      if (
-        req.headers["if-match"] && req.headers["if-match"] !== etag ||
-        (Date.parse(req.headers["if-unmodified-since"]) - stat.mtime.getTime()) < -2000
-      )
-        return forgetAboutIt(res, 412)
+      {
+        const ifMatch = req.headers["if-match"]
 
+        const ifUnmodifiedSince = req.headers["if-unmodified-since"]
+
+        if (
+          ifMatch && ifMatch !== etag ||
+          ifUnmodifiedSince && ( Date.parse(ifUnmodifiedSince) - stat.mtime.getTime() ) < -2000
+        )
+          return forgetAboutIt(res, 412)
+      }
 
       try {
 
-        await rangeRequest(options.baseDir + req.pathname, res, req.headers["range"], stat.size)
+        await rangeRequest(options.baseDir + pathname, res, req.headers["range"], stat.size)
 
       } catch (error) {
 
@@ -228,7 +192,7 @@ function range(options = {}) {
 
     try {
 
-      await streamIt(options.baseDir + req.pathname, res)
+      await streamIt(options.baseDir + pathname, res)
 
     } catch (error) {
 
@@ -240,18 +204,18 @@ function range(options = {}) {
 }
 
 
-function hush(res) {
+function hush(res: ServerResponse) {
   if (!res.headersSent) {
     res.statusCode = 500
     res.end()
   }
 }
 
-async function streamIt(path, res, opts) {
+async function streamIt(path: string, res: ServerResponse, opts?: { start: number, end: number }) {
   return pipelinePromised(
-    fs.createReadStream(path, opts ? { start: opts.start, end: opts.end} : null),
+    createReadStream(path, opts ? { start: opts.start, end: opts.end} : undefined),
     res,
-  ).catch(async (err) => {
+  ).catch(err => {
       if (!err || err.code === "ERR_STREAM_PREMATURE_CLOSE") // Stream closed (normal)
         return
       else
@@ -259,11 +223,11 @@ async function streamIt(path, res, opts) {
     })
 }
 
-function getEtag(mtime, size) {
+function getEtag(mtime: Date, size: number) {
   return `W/"${size.toString(16)}-${mtime.getTime().toString(16)}"`;
 }
 
-function forgetAboutIt(res, status) {
+function forgetAboutIt(res: ServerResponse, status: number) {
   res.removeHeader("content-type");
   res.removeHeader("content-length");
   res.removeHeader("cache-control");
@@ -271,38 +235,36 @@ function forgetAboutIt(res, status) {
   res.end();
 }
 
-function rangeRequest(path, res, range, size) {
+function rangeRequest(path: string, res: ServerResponse, rangeHeader: string, size: number) {
 
   res.removeHeader("content-length")
 
-  range = range.match(/bytes=([0-9]+)?-([0-9]+)?/i)
+  const rangeRegex = rangeHeader.match(/bytes=([0-9]+)?-([0-9]+)?/i)
 
-  if (!range) { // Incorrect pattren
+  if (!rangeRegex) { // Incorrect pattren
     res.setHeader("content-range", `bytes */${size}`)
     return forgetAboutIt(res, 416)
   }
 
-  let [start, end] = [range[1], range[2]]
+  let [ , start, end] = rangeRegex.map(n => Number(n))
 
   switch (true) {
 
-    case !!start && !end:   // Range: <unit>=<range-start>-
-      start = Number(start)
+    case !isNaN(start) && isNaN(end):   // Range: <unit>=<range-start>-
       end = size - 1
       break
 
-    case !start && !!end:   // Range: <unit>=-<suffix-length>
+    case isNaN(start) && !isNaN(end):   // Range: <unit>=-<suffix-length>
       start = size - end
       end = size - 1
       break
 
-    case !start && !end:    // Range: <unit>=-
+    case isNaN(start) && isNaN(end):    // Range: <unit>=-
       start = 0
       end = size - 1
       break
 
-    default:                // Range: <unit>=<range-start>-<range-end>
-      [start, end] = [Number(start), Number(end)]
+    // Default is Range: <unit>=<range-start>-<range-end>
   }
 
   if (start < 0 || start > end || end >= size) { // Range out of order or bigger than file size
@@ -315,10 +277,6 @@ function rangeRequest(path, res, range, size) {
   return streamIt(path, res, { start, end })
 }
 
-/**
- * @param {string} url
- * @returns {boolean}
- */
-function hasTrailingSlash(url) {
+function hasTrailingSlash(url: string): boolean {
   return url[url.length - 1] === "/"
 }
